9da2f0d1610356661c8d60c6fa1cea9b55558eac
FSWL <fswl98@gmail.com>
2020-06-21 14:37:16 +0300

    added consts

---
+++ src/consts/teams.js
@@ -0,0 +1,90 @@
+export const teams = [
+    {
+        name: '1/Unnamed_team',
+        members: [
+            {
+                firstName: 'Александра',
+                secondName: 'Василега',
+                city: 'Московская обл'
+            },
+            {
+                firstName: 'Роман',
+                secondName: 'Шевелев',
+                city: 'Кировская обл'
+            },
+            {
+                firstName: 'Дмитрий',
+                secondName: 'Урин',
+                city: 'Кировская обл'
+            },
+            {
+                firstName: 'Сергей',
+                secondName: 'Коротков',
+                city: 'Новосибирская обл'
+            },
+            {
+                firstName: 'Татьяна',
+                secondName: 'Черуканова',
+                city: 'Москва'
+            }
+        ],
+        place: 1
+    },
+    {
+        name: 'Uint2020&co',
+        members: [
+            {
+                firstName: 'Олег',
+                secondName: 'Игнатов',
+                city: 'Санкт-Петербург'
+            },
+            {
+                firstName: 'Кирилл',
+                secondName: 'Котов',
+                city: 'респ. Татарстан'
+            },
+            {
+                firstName: 'Евгений',
+                secondName: 'Рычков',
+                city: 'Москва'
+            },
+            {
+                firstName: 'Алексей',
+                secondName: 'Шаблий',
+                city: 'Москва'
+            },
+            {
+                firstName: 'Александр',
+                secondName: 'Зубарев',
+                city: 'Москва'
+            }
+        ],
+        place: 2
+    },
+    {
+        name: 'COSMOTEQ',
+        members: [
+            {
+                firstName: 'Павел',
+                secondName: 'Малышев',
+                city: 'Москва'
+            },
+            {
+                firstName: 'Егор',
+                secondName: 'Аксенов',
+                city: 'Москва'
+            },
+            {
+                firstName: 'Павел',
+                secondName: 'Андреев',
+                city: 'Москва'
+            },
+            {
+                firstName: 'Александр',
+                secondName: 'Андреев',
+                city: 'Москва'
+            }
+        ],
+        place: 3
+    }
+];
